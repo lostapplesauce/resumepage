a467337d59f36fbad874fde3aaf363d1dd248a55
troyturner <troyturner444@gmail.com>
2022-09-20 14:29:35 +0100

    changes

--- src/components/Bits.js
+++ src/components/Bits.js
@@ -11,33 +11,45 @@ export default function Bits() {
         return null
     }
     
-    return (
-      <Draggable>
-        <div className="Bits--container">
-            <div className="Bits--topBar">
-                    <button onClick={()=>setOpen(false)} className="Bits--closeWindow">x</button>
-            </div>        
-          <img
-            src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80"
-            alt="computer"
-          />
-          <img
-            src="https://images.unsplash.com/photo-1564865878688-9a244444042a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-            alt="phone with Eat Sleep Code Repeat on it"
-          />
-          <img
-            src="https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1625&q=80"
-            alt="computer with image of code on it"
-          />
-          <img
-            src="https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-            alt="lines of code at a 3/4 view"
-          />
-          <img
-            src={troyAndJenny}
-            alt="Of myself and Jenny"
-            />
+  return (
+    <Draggable defaultPosition={{ x: 200, y: 0 }}>
+      <div
+        className="Bits--container"
+      >
+        <div className="Bits--topBar">
+          <button onClick={() => setOpen(false)} className="Bits--closeWindow">
+            x
+          </button>
         </div>
-      </Draggable>
-    );
+        <img
+          src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80"
+          alt="computer"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1564865878688-9a244444042a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          alt="phone with Eat Sleep Code Repeat on it"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1625&q=80"
+          alt="computer with code on it"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          alt="lines of code at a 3/4 view"
+        />
+        <img src={troyAndJenny} alt="Of myself and Jenny" />
+      </div>
+    </Draggable>
+    // <Draggable>
+    //     <div className="box no-cursor" style={{display: 'flex', flexDirection: 'column'}}>
+    //       <strong className="cursor"><div>Drag here</div></strong>
+    //       <div style={{overflow: 'scroll'}}>
+    //         <div style={{background: 'yellow', whiteSpace: 'pre-wrap'}}>
+    //           I have long scrollable content with a handle
+    //           {'\n' + Array(40).fill('x').join('\n')}
+    //         </div>
+    //       </div>
+    //     </div>
+    // </Draggable>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../stylesheets/Header.css'
+import BandWTroy from '../images/bAndWTroy.jpg'
 
 export default function Header() {
   setInterval(function () {
@@ -8,8 +9,15 @@ export default function Header() {
   
     return (
       <header className="container">
-        <h1>Troy Turner</h1>
-        <div>
+        <div className="header--nameAndimageDiv">
+        <img
+          className="header--bAndWTroy"
+          src={BandWTroy}
+          alt="Me pretending to paint a masterpiece at Universal Studios "
+        />
+        <h1 className="header--name">Troy Turner</h1>
+        </div>
+        <div className="header--infoDiv">
           <h3>mobile: 077 470 71417</h3>
           <h3 className="email">email: troyturner444@gmail.com</h3>
           <div className="header--timeContainer">
