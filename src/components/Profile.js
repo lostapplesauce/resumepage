15e485c75d292ac185a1937a096ee050281bff83
troyturner <troyturner444@gmail.com>
2022-09-20 15:53:54 +0100

    updated about me section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,31 +6,37 @@ export default function Proifle() {
    
     
     return (
-        <div className="profile--container">
-            <h2 className="profile--title">About Me</h2>
-            <p className="profile--description">
-                <strong>Currenlty I am residing in Scotland and trying to get a working visa.</strong>
-                <hr/>
-                Enjoy being able to travel around and see all this beautiful planet offers.
-                On top of traveling the opportunity to eat in various countries is amazing! 
-                Love trying various cuisines and finding new dishes. During my free time, when at home, 
-                been working on myself by improving programming skills with lessons online 
-                and practice. 
-                <br />
-                Have a good amount of expierence working within a team. Through out my life I 
-                have played variout sports from football to American football. Having also served
-                in the military instilled, initially, the skillset to work witin a team at work.
-                When a groupd of peope are working in sync and getting things accomplished it 
-                creates an undescribable joy within myself. Teamwork makes the dream work.
-                <br />
-                When I have time I enjoying diving into art and getting the creative juices flowing.
-                The moment when something of my imagination goes on to the page and looks remotely
-                close is always an accomplishment, I am not the greatest artist but still find joy in it.
-                Doing frontend web development work feels like an art project to me, being able to add
-                components and make them come alive.                 
-            </p>
-            
-            <WindowTracker />
-        </div>
-    )
+      <div className="profile--container">
+        <h2 className="profile--title">About Me</h2>
+        <p className="profile--description">
+          <strong>
+            Currenlty I am residing in Scotland and trying to get a working
+            visa.
+          </strong>
+          <hr />
+          I enjoy being able to travel around and see all this beautiful planet
+          offers. On top of traveling, the opportunity to eat in various
+          countries is amazing! In addition I love trying various cuisines and
+          finding new dishes. During my free time, when at home, I have been
+          working on myself by improving programming skills with lessons online
+          and practice.
+          <br />
+          Being a good team player is one of my greatest assets. Throughout my
+          life I have played various sports, from football to American football,
+          which initally instilled that teamwork spirit. Having also served in the military
+          further strengthened a team mindset. When a group of
+          people are working in sync and getting things accomplished it creates
+          an undescribable joy within myself. Teamwork makes the dream work.
+          <br />
+          When I have time I enjoy diving into art and getting the creative
+          juices flowing. The moment when something of my imagination goes on to
+          the page and looks remotely close, it is always such an accomplishment. I may
+          not be the best artist but still find great joy within it. Doing frontend web
+          development work feels like an art project to me, being able to add
+          components and make them come alive.
+        </p>
+
+        <WindowTracker />
+      </div>
+    );
 }
